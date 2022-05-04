375edd44eea1db0076b5081a355164e6b8baaf56
peyman <lord.akinak@gmail.com>
2022-05-04 18:45:13 +0430

    fix

--- src/Services/GivSyncronizer.php
+++ src/Services/GivSyncronizer.php
@@ -112,6 +112,7 @@ class GivSyncronizer
         // use this to keep other parts of category data untouched
         $getUpdateCategoryAttrs = function ($givCat, $parentId, $data) {
             return [
+                'name' => 'giv-'.$givCat->CategoryCode,
                 'author_id' => config('larapress.giv.author_id'),
                 'deleted_at' => $givCat->CategoryIsActive ? null : Carbon::now(),
                 'parent_id' => $parentId,
